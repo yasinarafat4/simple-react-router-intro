3d2dcd6298c82dbee223c9b8c325fe53d5a6d28a
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-04 09:18:09 +0600

    done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <Header></Header>
       <h3>This is home</h3>
+      <div
+        style={{
+          color: "red",
+          fontSize: "40px",
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        {navigation.state === "loading" && "Loading..."}
+      </div>
       <Outlet />
     </div>
   );
